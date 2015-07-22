299876df9fad62c0b575cc1a06ed4aa6581509e5
pvsmartinez <pvsmartinez@gmail.com>
2015-07-22 14:47:14 -0300

    stuff

--- controller/os.js
+++ controller/os.js
@@ -1,12 +1,17 @@
 module.exports = {
-	map : map
+    map: map
 }
 
 function map() {
-	global.app.get('/os', global.checkAuth([1,2,3,4]), function (req, res, next) {
-		global.db.query(global.services.os.listAll(), function(err, rows) {
-			res.render('os', {locals: {os:rows}});
-		});
-		
-	});
+    global.app.get('/os', global.checkAuth([1, 2, 3, 4]), function(req, res, next) {
+        global.db.query(global.services.os.listAll(), function(err, rows) {
+        	console.log(rows);
+            res.render('os', {
+                locals: {
+                    orders: rows
+                }
+            });
+        });
+
+    });
 }
