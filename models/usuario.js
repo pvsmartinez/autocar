23eb06797bc4d0936ac15c8e2ce985d4abc5f8fd
eduardolevygp <eduardo.levygp@gmail.com>
2015-07-28 09:48:55 -0300

    crud de especialidades

--- models/usuario.js
+++ models/usuario.js
@@ -13,18 +13,18 @@ var obj = {
 	telefone : "varchar(255)",
 	endereco : "varchar(255)",
 	permissao : "int(1) not null default 0",
-	especialidade : "int(11) null",
-	__foreignKeys : ["(especialidade) REFERENCES especialidade(id)"]
+	especialidade_id : "int(11) null",
+	__foreignKeys : ["(especialidade_id) REFERENCES especialidade(id)"]
 }
 
 var objs = [
 	{email: "cliente@usp.br", senha : "1234", nome : "cliente", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"0"},
 	{email: "atendente@usp.br", senha : "1234", nome : "atendente", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"1"},
 	{email: "tecnico@usp.br", senha : "1234", nome : "tecnico", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"2"},
-	{email: "mecanico@usp.br", senha : "1234", nome : "mecanico", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade : "1"},
-	{email: "mecanico2@usp.br", senha : "1234", nome : "mecanico2", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade : "2"},
-	{email: "mecanic3@usp.br", senha : "1234", nome : "mecanico3", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade : "1"},
-	{email: "mecanico4@usp.br", senha : "1234", nome : "mecanico4", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade : "2"},
+	{email: "mecanico@usp.br", senha : "1234", nome : "mecanico", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade_id : "1"},
+	{email: "mecanico2@usp.br", senha : "1234", nome : "mecanico2", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade_id : "2"},
+	{email: "mecanic3@usp.br", senha : "1234", nome : "mecanico3", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade_id : "1"},
+	{email: "mecanico4@usp.br", senha : "1234", nome : "mecanico4", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"3", especialidade_id : "2"},
 	{email: "administrador@usp.br", senha : "1234", nome : "administrador", telefone : "12345678", endereco : "Av Luciano Gualberto, 123", permissao :"4"}
 ];
 
